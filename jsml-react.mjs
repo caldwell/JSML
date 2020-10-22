8747d3a95bd518db09776f2fad487efbbde812f8
David Caldwell <david@porkrind.org>
2020-10-21 18:59:49 -0700

    Add jsml-react.mjs: Build react structures from jsml instead of jsx

---
+++ jsml-react.mjs
@@ -0,0 +1,51 @@
+// Copyright (c) 2009-2020 David Caldwell and Jim Radford.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import { Fragment, createElement } from 'react';
+
+function jsr(array) {
+    var valid = (a, array) => a !== undefined && a !== null || (() => { throw("undefined value in: " + JSON.stringify(array)) })();
+    let react_ignored = (a) => a == undefined || a == null || a === false || a === true;
+
+    if (array.constructor === String) return array.toString(); // allows for jsml("Plain text") (which is only valid in react when recursing)
+    if (array.$$typeof == Symbol.for('react.element')) return createElement(Fragment, null, array); // jsr([jsr(['div'])])
+    if (!array.length) return []; // [['div'],[]] => [['div']]
+
+    if (array[0].constructor === Array || array[0].$$typeof == Symbol.for('react.element')) { // [[],[],...] ==> [],[],...
+        return createElement(Fragment, null, ...array.map((e) => jsr(e)));
+    }
+
+    var el = array[0];
+    var props = {};
+    var children = [];
+
+    for (var a of array.slice(1)) {
+        if (react_ignored(a))
+            continue;
+        else if (a.constructor === Array) {
+            if (a.length !== 0)
+                children.push(jsr(a));
+        }
+        else if (a.$$typeof == Symbol.for('react.element'))
+            children.push(a);
+        else if (a.constructor === Object)
+            for (let [k,v] of Object.entries(a)) {
+                if (valid(v,a) && v.constructor === Object) {
+                    props[k] = {};
+                    for (let [sk,sv] of Object.entries(v))
+                        props[k][sk] = sv;
+                }
+                else
+                    props[k] = v;
+            }
+        else if (typeof a  === "function")
+            props.ref = a; // ['div', _=>some_var=_ ] save reference to deep element
+        else
+            children.push(a.toString());
+    }
+    return createElement(el, props, ...children);
+}
+
+export { jsr };
